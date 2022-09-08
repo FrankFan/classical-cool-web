1007c73f510b3cad266878ebbb7aacb13f2fa0d8
awen.qiu <awen.qiu@yijinin.com>
2022-09-09 00:44:19 +0800

    feat:页面

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FC } from 'react';
 import './index.less';
 import coverImg from '@/assets/img/1.png';
+import titleImg from '@/assets/img/icon-1.png';
 
 interface BookListProps {}
 
@@ -15,9 +16,22 @@ const BookList: FC<BookListProps> = () => {
         <div className="left">
           <img src={coverImg} alt="" className="cover" />
           <div className="content">
-            <p>孔子</p>
-            <article>1233333333</article>
-            <a href="" className="btn-box">查看详情</a>
+            <p className="title">
+              <img className="title-icon" src={titleImg} alt="" />
+              <span>需要个标题（热卖中）</span>
+            </p>
+            <p className="author">孔子</p>
+            <article>
+              古圣先贤的智慧文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案古圣先贤的智慧文案文案文案文案文案文案文案文案文案文案文案文案
+            </article>
+            <a href="" className="btn-box">
+              查看详情
+            </a>
+            <div className="visitor">
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
           </div>
         </div>
       </div>

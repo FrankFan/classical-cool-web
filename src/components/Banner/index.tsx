01c916ee99a39a7bcda5cd1d430d08c0d805442d
FrankFan <fanyong@gmail.com>
2022-09-22 19:56:49 +0800

    feat: update

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -14,7 +14,7 @@ interface BannerProps {}
 
 const Banner: FC<BannerProps> = () => {
   return (
-    <div className="banner">
+    <div className='banner'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -24,36 +24,35 @@ const Banner: FC<BannerProps> = () => {
         //   disableOnInteraction: false
         // }}
         pagination={{
-          clickable: true
+          clickable: true,
         }}
         // navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
+        className='mySwiper'
       >
         <SwiperSlide>
-          <img src={banner1} alt="" />
+          <img src={banner1} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={banner2} alt="" />
+          <img src={banner2} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={banner3} alt="" />
+          <img src={banner3} alt='' />
         </SwiperSlide>
         <SwiperSlide>
           <a
-            data-title="书房展"
-            href="/show/258010.html"
-            target="_blank"
+            data-title='书房展'
+            href='/show/258010.html'
+            target='_blank'
             // style="background:url(/Uploads/Picture/2022/08/29/s630c6830d3285.jpg) no-repeat center;background-size:auto 100%;/*cursor:pointer;*/"
           >
             <img
-              src="https://img.dpm.org.cn/Uploads/Picture/2022/08/29/s630c6830d3285.jpg"
-              alt=""
+              src='https://img.dpm.org.cn/Uploads/Picture/2022/08/29/s630c6830d3285.jpg'
+              alt=''
             />
           </a>
         </SwiperSlide>
       </Swiper>
-      <Mint></Mint>
     </div>
   );
 };

12242ddce43412c9761c1b09c1dbcd193e957891
jeremy.su <jeremy.su@yijinin.com>
2022-09-22 14:53:27 +0800

    feat: add book data

---
+++ src/data/books.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 1,
+    itemId: "_JFTgEnuWIpp2qWxMv9Fm1_aghO_gUbeim_rZKavS1I",
+    title: "山海经",
+  },
+  {
+    id: 2,
+    itemId: "iQiR-Gm1Gbbq3_plxj_HMhe1ipKxzqChNyVRLc1TmKI",
+    title: "孙子兵法",
+  },
+];
--- src/pages/$id.tsx
+++ src/pages/$id.tsx
@@ -15,6 +15,8 @@ const Detail: FC<DetailProps> = () => {
   const markdowns = useFetchMDContents(files);
   console.log(markdowns);
 
+  if (!markdowns.length) return null;
+
   return (
     <HTMLFlipBook
       className="flip-book"
@@ -33,72 +35,26 @@ const Detail: FC<DetailProps> = () => {
           <h2>BOOK TITLE</h2>
         </div>
       </div>
-      <div className="page">
-        <div className="page-content">
-          <h2 className="page-header">Page header 1</h2>
-          <div className="page-text">
-            {markdowns.length > 0
-              ? markdowns.map((md, idx) => {
-                  return <ReactMarkdown key={idx}>{md}</ReactMarkdown>;
-                })
-              : null}
+      {markdowns.map((md, idx) => {
+        return (
+          <div className="page" key={idx}>
+            <div className="page-content">
+              <h2 className="page-header">Page header {idx + 1}</h2>
+              <div className="page-text">
+                <ReactMarkdown key={idx}>{md}</ReactMarkdown>
+              </div>
+              <div className="page-footer">2</div>
+            </div>
           </div>
-          <div className="page-footer">2</div>
-        </div>
-      </div>
-      <div className="page">
-        <div className="page-content">
-          <h2 className="page-header">Page header - 15</h2>
-          {/* <div class="page-image" style="background-image: url(images/html/7.jpg)"></div> */}
-          <div className="page-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus
-            mollis nibh, non convallis ex convallis eu. Suspendisse potenti.
-            Aenean vitae pellentesque erat. Integer non tristique quam.
-            Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra
-            metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus,
-            accumsan eros sed, viverra enim. Pellentesque non justo vel nibh
-            sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. In cursus mollis nibh, non convallis ex
-            convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat.
-            Integer non tristique quam. Suspendisse rutrum, augue ac
-            sollicitudin mollis, eros velit viverra metus, a venenatis tellus
-            tellus id magna.
-          </div>
-          <div className="page-footer">16</div>
-        </div>
-      </div>
-      <div className="page">
-        <div className="page-content">
-          <h2 className="page-header">Page header - 16</h2>
-          {/* <div class="page-image" style="background-image: url(images/html/8.jpg)"></div> */}
-          <div className="page-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus
-            mollis nibh, non convallis ex convallis eu. Suspendisse potenti.
-            Aenean vitae pellentesque erat. Integer non tristique quam.
-            Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra
-            metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus,
-            accumsan eros sed, viverra enim. Pellentesque non justo vel nibh
-            sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. In cursus mollis nibh, non convallis ex
-            convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat.
-            Integer non tristique quam. Suspendisse rutrum, augue ac
-            sollicitudin mollis, eros velit viverra metus, a venenatis tellus
-            tellus id magna.
-          </div>
-          <div className="page-footer">17</div>
-        </div>
-      </div>
+        );
+      })}
+
       <div className="page page-cover page-cover-bottom" data-density="hard">
         <div className="page-content">
           <h2>THE END</h2>
         </div>
       </div>
     </HTMLFlipBook>
-    // <div className="container">
-    //   <div className="flip-book" id="demoBookExample">
-
-    //   </div>
-    // </div>
   );
 };
 

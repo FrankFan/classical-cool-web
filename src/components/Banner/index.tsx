55d8bb691cc5141eaa1e29f1a141b0f0071e97cd
FrankFan <fanyong@gmail.com>
2022-12-30 22:27:04 +0800

    feat: 压缩图片体积

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { FC } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "./index.less";
-import banner1 from "@/assets/img/banner1.png";
-import banner2 from "@/assets/img/banner1.jpg";
-import banner3 from "@/assets/img/banner2.jpg";
-import { Autoplay, Pagination, Navigation } from "swiper";
-import Mint from "../Mint";
+import React from 'react';
+import { FC } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './index.less';
+import banner1 from '@/assets/img/banner1.png';
+import banner2 from '@/assets/img/banner2.jpeg';
+import banner3 from '@/assets/img/banner3.jpeg';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import Mint from '../Mint';
 interface BannerProps {}
 
 const Banner: FC<BannerProps> = () => {
   return (
-    <div className="banner">
+    <div className='banner' id='top'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -28,38 +28,16 @@ const Banner: FC<BannerProps> = () => {
         }}
         // navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
+        className='mySwiper'
       >
         <SwiperSlide>
-          <img
-            src="https://arseed.web3infra.dev/w8JKZX99SX8clktRcOKCAyFOLjn9xcWtqvhQZYvgY8s"
-            alt=""
-          />
+          <img src={banner1} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://arseed.web3infra.dev/Tif-2ZPQtMwm2GL1kLlf90NHq0DqPLAJO7k22UBtAso"
-            alt=""
-          />
+          <img src={banner2} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://arseed.web3infra.dev/QOf1TQJ1OXnVpoWD9BwAQcfpzwpN4EveIPH3ujrJwGA"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            data-title="书房展"
-            href="/show/258010.html"
-            target="_blank"
-            // style="background:url(/Uploads/Picture/2022/08/29/s630c6830d3285.jpg) no-repeat center;background-size:auto 100%;/*cursor:pointer;*/"
-          >
-            <img
-              src="https://arseed.web3infra.dev/CLoMVmYZcfmb9xSAitjyvfnWqdtZLQSqUbAp5LRcwiE"
-              alt=""
-            />
-          </a>
+          <img src={banner3} alt='' />
         </SwiperSlide>
       </Swiper>
     </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { FC } from "react";
-import ConnectWalletButton from "../ConnectWalletButton";
+import React from 'react';
+import { FC } from 'react';
+import ConnectWalletButton from '../ConnectWalletButton';
 
-import "./index.less";
+import './index.less';
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
   return (
-    <header className="header">
-      <div className="logo">
+    <header className='header'>
+      <div className='logo'>
         <img
-          src="https://arseed.web3infra.dev/o81fZcXnJJYZrj4S5INq7OUhUI8I4uWOOK9NS2M8ixw"
-          alt=""
+          src='https://arseed.web3infra.dev/o81fZcXnJJYZrj4S5INq7OUhUI8I4uWOOK9NS2M8ixw'
+          alt=''
         />
       </div>
       <nav>
         <ul>
           <li>
-            <a href="#">首页</a>
+            <a href='#top'>首页</a>
           </li>
-          <li className="">
-            <a href="#booklist">活动</a>
+          <li className=''>
+            <a href='#booklist'>活动</a>
           </li>
-          <li className="">
-            <a href="#activity">记录</a>
+          <li className=''>
+            <a href='#activity'>记录</a>
           </li>
-          <li className="">
+          {/* <li className="">
             <a href="#history">历史</a>
-          </li>
+          </li> */}
         </ul>
       </nav>
-      <div className="btn">
+      <div className='btn'>
         <ConnectWalletButton></ConnectWalletButton>
       </div>
     </header>

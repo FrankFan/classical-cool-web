a004b2547273ee24347ac524cbb2df3fc1b6c54e
FrankFan <fanyong@gmail.com>
2023-01-14 22:27:35 +0800

    feat: add footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import { FC } from "react";
+import React from 'react'
+import { FC } from 'react'
+import './index.less'
+import logo from '@/assets/logo/logo2.svg'
 
 interface FooterProps {}
 
-const Footer: FC<FooterProps> = () => {
-  return <div>Footer</div>;
-};
+const year = new Date().getFullYear()
 
-export default Footer;
+const Footer: FC<FooterProps> = () => {
+  return (
+    <>
+      <div className='footer'>
+        <div className='footer_left'>
+          <img src={logo} alt='' />
+        </div>
+        <div className='footer_right'>
+          <a target='_blank' href='https://fo8q1pmkw7j.typeform.com/to/Dt8LjRmJ'>
+            合作申请
+          </a>
+          <a target='_blank' href='https://github.com/classical-cool'>
+            Github
+          </a>
+          <a target='_blank' href='https://twitter.com/classical-cool'>
+            Twitter
+          </a>
+        </div>
+      </div>
+      <div className='copyright'>©{year} Classical.cool All Rights Reserved</div>
+    </>
+  )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Activity from '@/components/Activity'
 import Banner from '@/components/Banner'
 import BookList from '@/components/BookList'
 // import BookShow from "@/components/BookShow";
-import ConnectWalletButton from '@/components/ConnectWalletButton'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import { MobileHeader } from '@/components/MobileHeader'
@@ -12,15 +11,14 @@ import './index.less'
 export default function HomePage() {
   return (
     <div className='home-box'>
-      <Header></Header>
+      <Header />
       <MobileHeader />
-      <Banner></Banner>
-      <BookList></BookList>
+      <Banner />
+      <BookList />
       {/* <BookShow></BookShow> */}
       <Activity></Activity>
-      {/* <Record></Record>
-      <Footer></Footer> */}
-      {/* <ConnectWalletButton></ConnectWalletButton> */}
+      {/* <Record></Record>*/}
+      <Footer />
     </div>
   )
 }

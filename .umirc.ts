6115832b4b4bf1114fed041fa837e01ea0bdc959
FrankFan <fanyong@gmail.com>
2023-01-18 17:50:27 +0800

    feat: fix build problem

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,7 @@
 export default {
-  npmClient: "yarn",
+  npmClient: 'yarn',
   hash: true,
-};
+
+  // fix ERROR: Big integer literals are not available in the configured target environment ("chrome80", "es2015")
+  jsMinifier: 'terser',
+}
